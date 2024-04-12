8f5cdab20085d90e44974416ba9e7e06a9831a32
Marcus Benoit <benoit.marcus@gmail.com>
2024-04-12 17:35:18 +0200

    initial commit - @Observable is used when a view should be updated after a class change because Swift doesn't track it for classes (only structs)

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,15 +7,28 @@
 
 import SwiftUI
 
+@Observable // added so swift can observe if the instance of a class changes, @state doesn't check for that https://www.hackingwithswift.com/books/ios-swiftui/using-state-with-classes
+class User {
+    var firstName = "Marco"
+    var lastName = "Reus"
+}
+
 struct ContentView: View {
+    @State private var user = User()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Your name is \(user.firstName) \(user.lastName).")
+            
+            TextField("First name:", text: $user.firstName)
+                .padding(10)
+                .background(.green)
+            TextField("Last name:", text: $user.lastName)
+                .padding(10)
+                .background(.gray)
         }
         .padding()
+        .background(.yellow)
     }
 }
 
