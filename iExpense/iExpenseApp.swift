ce5b7640a222b0848158ee3a60222355d39c9a2c
Marcus Benoit <benoit.marcus@gmail.com>
2024-04-12 16:08:46 +0200

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Marcus Benoit on 12.04.24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
