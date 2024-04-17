76c6ea42f05334d07d96bcff44616589c851f3a8
Marcus Benoit <benoit.marcus@gmail.com>
2024-04-17 10:18:34 +0200

    Project 7 - Challenge 1 - using the preferred currency - done

---
+++ iExpense/AddView.swift
@@ -0,0 +1,18 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Marcus Benoit on 16.04.24.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddView()
+}
